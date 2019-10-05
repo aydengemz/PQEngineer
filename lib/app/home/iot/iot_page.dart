5b4dbb7a96eb3e6eec5b66eb6950228cc2277dd2
Ayden Xu <aydenxunc@gmail.com>
2019-10-05 13:36:26 -0700

    1005-136pm-SensorDatatoEdge

--- lib/app/home/iot/iot_page.dart
+++ lib/app/home/iot/iot_page.dart
@@ -3,13 +3,13 @@ import 'package:fiveg/app/home/home_page.dart';
 import 'package:fiveg/app/home/iot/battery_page.dart';
 import 'package:fiveg/app/home/iot/phoneinfo_page.dart';
 import 'package:fiveg/app/home/iot/sensor_page.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/utils.dart';
 import '../../../common_widgets/list_widget.dart';
 import 'index.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class AnimationsPlayground extends StatelessWidget {
   static Route<dynamic> route() {
@@ -40,34 +40,7 @@ class _IoTPageState extends State<IoTPage> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     //double height = (MediaQuery.of(context).size.height / 3) -
     //16 -
-    // kBottomNavigationBarHeight;
-
-
-final controller = ScrollController();
-
-
-double cWidth = 0.0;
-  double itemHeight = MediaQuery.of(context).size.height * 0.3;
-  double itemsCount = 3;
-  double screenWidth;
-screenWidth = MediaQuery.of(context).size.height;
- 
-  onScroll() {
-    setState(() {
-      cWidth = controller.offset * screenWidth / (itemHeight * itemsCount);
-    });
-  }
-   @override
-  void initState() {
-    super.initState();
-    controller.addListener(onScroll);
-  }
-  @override
-void dispose() {
-  controller.removeListener(onScroll);
-}
-
- 
+    // kBottomNavigationBarHeight
 
     final images = ['images/s10.jpeg', 'images/Sensors.jpg', 'images/s10.jpeg'];
     final pages = [PhoneInfo(), SensorPage(), BatteryPage()];
@@ -80,120 +53,163 @@ void dispose() {
           children: <Widget>[
             Center(
               child: UserAccountsDrawerHeader(
-  accountName: Text("Ayden Xu"),
-  accountEmail: Text("Ayden@5g.com"),
-  currentAccountPicture: CircleAvatar(
-    backgroundColor:
-        Theme.of(context).platform == TargetPlatform.iOS
-              ? Color(0xff70a0ff)
-              : Colors.white,
-    child: Text(
-      "A",
-      style: TextStyle(fontSize: 40.0),
-    ),
-  ),
-),
+                accountName: Text("Ayden Xu"),
+                accountEmail: Text("Ayden@5g.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor:
+                      Theme.of(context).platform == TargetPlatform.iOS
+                          ? Color(0xff70a0ff)
+                          : Colors.white,
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+              ),
             ),
-            ],
+          ],
         ),
       ),
       body: Stack(
-        
         children: <Widget>[
-
           Container(
             height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(color: Color(0xff70a0ff)),
             child: Padding(
               padding: const EdgeInsets.only(top: 100.0, bottom: 20),
-                          child: Center(
+              child: Center(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   //mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
                     Padding(
-                      padding: const EdgeInsets.only(top: 30.0, bottom: 10),
-                      child: Center(child: Text("Production Quality Engineer", style: TextStyle(color: Colors.white, fontSize: 27, fontWeight: FontWeight.w400),)),
-                    ),
-                     Padding(
-                      padding: const EdgeInsets.only(top: 10.0, bottom: 40, left: 30),
-                      child: Center(child: Text("Utilizing 5G /n/n Faster production quality inspections", style: TextStyle(color: Colors.white, fontSize: 17.5, fontWeight: FontWeight.w300),)),
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.all(
+                                                const Radius.circular(16)),
+                                            color: Colors.white,
+                                            
+                                          ),
+                       
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 30.0, bottom: 10),
+                              child: Center(
+                                  child: Text(
+                                "Production Quality Engineer",
+                                style: TextStyle(
+                                    color: Color(0xff70a0ff),
+                                    fontSize: 27,
+                                    fontWeight: FontWeight.w400),
+                              )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10.0, bottom: 40, left: 30),
+                              child: Center(
+                                  child: Text(
+                                "Utilize 5G, Faster production quality inspections",
+                                style: TextStyle(
+                                    color: Color(0xff70a0ff),
+                                    fontSize: 17.5,
+                                    fontWeight: FontWeight.w300),
+                              )),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                     Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 30.0),
-                          child: Container(
-                          alignment: Alignment.center,
-                          height: MediaQuery.of(context).size.height * 0.3,
-                          child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 3,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                    width: MediaQuery.of(context).size.width * 0.85,
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            const Radius.circular(16)),
-                                        color: Colors.transparent,
-                                      ),
-                                      child: Container(
-                                          padding: EdgeInsets.only(left: 20),
-                                          child: Center(
-                                              child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: <Widget>[
-                                              Expanded(
-                                                child: GestureDetector(
-                                                  onTap: () {
-                                                    Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              pages[index],
-                                                        ));
-                                                  },
-                                                  child: Container(
-                                                    //alignment: Alignment.bottomLeft,
-                                                    //padding: EdgeInsets.all(16),
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 20.0),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: MediaQuery.of(context).size.height * 0.3,
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 3,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.85,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.all(
+                                                const Radius.circular(16)),
+                                            color: Colors.transparent,
+                                          ),
+                                          child: Container(
+                                              padding:
+                                                  EdgeInsets.only(left: 20),
+                                              child: Center(
+                                                  child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: <Widget>[
+                                                  Expanded(
+                                                    child: GestureDetector(
+                                                      onTap: () {
+                                                        Navigator.push(
+                                                            context,
+                                                            MaterialPageRoute(
+                                                              builder:
+                                                                  (context) =>
+                                                                      pages[
+                                                                          index],
+                                                            ));
+                                                      },
+                                                      child: Container(
+                                                        //alignment: Alignment.bottomLeft,
+                                                        //padding: EdgeInsets.all(16),
 
-                                                    decoration: BoxDecoration(
-                                                      borderRadius: BorderRadius.all(
-                                                          const Radius.circular(16)),
-                                                      image: DecorationImage(
-                                                        image:
-                                                            AssetImage(images[index]),
-                                                        fit: BoxFit.cover,
+                                                        decoration:
+                                                            BoxDecoration(
+                                                          borderRadius:
+                                                              BorderRadius.all(
+                                                                  const Radius
+                                                                          .circular(
+                                                                      16)),
+                                                          image:
+                                                              DecorationImage(
+                                                            image: AssetImage(
+                                                                images[index]),
+                                                            fit: BoxFit.cover,
+                                                          ),
+                                                        ),
+                                                        child: Column(
+                                                          mainAxisAlignment:
+                                                              MainAxisAlignment
+                                                                  .center,
+                                                          crossAxisAlignment:
+                                                              CrossAxisAlignment
+                                                                  .stretch,
+                                                          children: <Widget>[
+                                                            /*   Icon(Icons.phone_android),
+                                        Center(child: Text("Phone Info"))*/
+                                                            SizedBox(
+                                                              height: 10,
+                                                            )
+                                                          ],
+                                                        ),
                                                       ),
                                                     ),
-                                                    child: Column(
-                                                      mainAxisAlignment:
-                                                          MainAxisAlignment.center,
-                                                      crossAxisAlignment:
-                                                          CrossAxisAlignment.stretch,
-                                                      children: <Widget>[
-                                                        /*   Icon(Icons.phone_android),
-                                        Center(child: Text("Phone Info"))*/
-                                                        SizedBox(
-                                                          height: 10,
-                                                        )
-                                                      ],
-                                                    ),
                                                   ),
-                                                ),
-                                              ),
-                                            ],
-                                          ))),
-                                    ));
-                              }),
-                      ),
-                        ),
-                      ]),
+                                                ],
+                                              ))),
+                                        ));
+                                  }),
+                            ),
+                          ),
+                        ]),
                   ],
                 ),
               ),
@@ -224,9 +240,7 @@ void dispose() {
               elevation: 0.0, //Shadow gone
             ),
           ),
-                    ],
-                                    
-      
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:fiveg/app/landing_page.dart';
 import 'package:fiveg/services/auth.dart';
 
+import 'package:overlay_support/overlay_support.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,23 +15,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Provider<AuthBase>(
       builder: (context) => Auth(),
-      child: MaterialApp(
-         onGenerateRoute: (RouteSettings settings) {
-        if (settings.isInitialRoute) {
-          return AnimationsPlayground.route();
-        }
-        return null;
-      },
-        title: 'PQ Engineer',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-         
-           textTheme: TextTheme(
-            body1: TextStyle(fontSize: 20, color: Colors.blue),
+      child: OverlaySupport(
+              child: MaterialApp(
+           onGenerateRoute: (RouteSettings settings) {
+          if (settings.isInitialRoute) {
+            return AnimationsPlayground.route();
+          }
+          return null;
+        },
+          title: 'PQ Engineer',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+           
+             textTheme: TextTheme(
+              body1: TextStyle(fontSize: 20, color: Colors.blue),
     ),
-            iconTheme: IconThemeData(color: Colors.blue),
+              iconTheme: IconThemeData(color: Colors.blue),
+          ),
+          home: LandingPage(),
         ),
-        home: LandingPage(),
       ),
     );
   }
